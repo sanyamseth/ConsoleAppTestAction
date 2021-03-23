aa0eecbf8ae7469e44b1ec2ab57bdf5d26202272
Sanyam Seth <saseth@microsoft.com>
2021-03-23 16:36:05 +0530

    Changes for app

--- GitHubAction/GitHubAction/Program.cs
+++ GitHubAction/GitHubAction/Program.cs
@@ -5,7 +5,13 @@ namespace GitHubAction
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+
+
+            foreach(var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
             Console.WriteLine("Hello World2!");
            
         }
